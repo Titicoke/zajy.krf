f607be183be3d67337a4367690698e37b8b850fe
孔瑞锋 <3663929@qq.com>
2025-03-31 17:44:57 +0800

    ddd

---
+++ my-thinkphp-app/app/admin/controller/index.php
@@ -0,0 +1,9 @@
+<?php
+namespace app\admin\controller;
+use app\BaseController;
+
+class index extends BaseController{
+    public function index(){
+        return 'admin模块下的index控制器下的index方法';
+    }
+}
